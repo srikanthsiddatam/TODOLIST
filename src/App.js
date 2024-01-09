86802515646bfdb7214d4d27b7245b25ceec1149
srikanthsiddatam <siddatamsrikanth7@gmail.com>
2024-01-09 08:46:43 +0530

    TODOLIST

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+const [todoinput,settodoinput]=useState("")
+  const [todo,settodo]=useState([
+    {
+      id:1,
+      task:"learn react"
+    },
+    {
+      id:2,
+      task:"learn Angular"
+    },
+    // {
+    //   id:3,
+    //   task:"learn Web development"
+    // }
+
+    
+
+  ])
+
+  let nexttodo=3
+  const changetodo=()=>{
+    if(todoinput===""){
+      alert("add some task")
+    }
+    else{
+      const newtodos=[...todo
+      ,
+      {
+        id: nexttodo++,
+        task:todoinput
+      }
+    
+    ]
+    settodo(newtodos)
+    settodoinput("")
+      
+    }
+  }
+  const delete11=(id)=>{
+  let updatedtodos=  todo.fliter((todo1)=>{
+      return todo1.id != id
+
+    })
+
+    updatedtodos(settodo)
+
+  }
+
+
+
+
+
+
+  return(
+    <>
+
+    
+    <div className='container mt-5 '>
+
+      <h1>TODO APP USING REACT</h1>
+        <div className='input-group'>
+               <input className='form-control' type="text" onChange={(e)=>{
+                let task=e.target.value
+                settodoinput(task)
+               }} value={todoinput} />
+               <button className='btn btn-primary' onClick={changetodo}>Add</button>
+      </div>
+      <ul className='list-group mt-5'>
+       { todo.map((item)=>{
+        return <li className='list-group-item'>
+        <p>{item.task}</p> 
+        <button type="button" className='btn btn-primary' onClick={delete11}>❌</button>
+      </li>
+
+
+
+        })}
+        
+        
+      </ul>
     </div>
-  );
+   
+    
+    </>
+  )
 }
-
-export default App;
+export default App
